5e9eb80c9e75bf632096b3cdb46ed9924b405003
70NA5 <56596204+70NA5@users.noreply.github.com>
2023-03-13 10:49:39 +0100

    added simple script for laser

--- Assets/Scripts/ARPLaser.cs
+++ Assets/Scripts/ARPLaser.cs
@@ -1,32 +1,35 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.Net.Mail;
 using UnityEngine;
 
 
-public class ARPLaser : MonoBehaviour
-    private LineRenderer lr;
-[Serializefield]
-privare startPoint;
+public class ARPLaser : MonoBehaviour 
 {
+    private LineRenderer lr;
+    [SerializeField]
+    private Transform startPoint;
+
     // Start is called before the first frame update
     void Start()
     {
-        lr = GetComponent<LineRenderer> 
+        lr = GetComponent<LineRenderer>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        lr.setPosition(0, startPoint.position);
+        lr.SetPosition(0, startPoint.position);
         RaycastHit hit;
         if (Physics.Raycast(transform.position, -transform.right, out hit))
         {
             if (hit.collider)
             {
-                lr.setPosition(1, hit.point);
+                lr.SetPosition(1, hit.point);
             }
-        } else lr.setPosition(1, -transform.right * 5000)
+        }
+        else lr.SetPosition(1, -transform.right * 5000);
     } 
 
 }
