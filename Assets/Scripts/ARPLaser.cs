59838ad44ae3720d9cb78be50484ac2c20cb6ec1
70NA5 <56596204+70NA5@users.noreply.github.com>
2023-03-13 12:30:12 +0100

    added simple logic for colour change

--- Assets/Scripts/ARPLaser.cs
+++ Assets/Scripts/ARPLaser.cs
@@ -10,6 +10,7 @@ public class ARPLaser : MonoBehaviour
     private LineRenderer lr;
     [SerializeField]
     private Transform startPoint;
+    public Material mat1, mat2;
 
     // Start is called before the first frame update
     void Start()
@@ -24,9 +25,22 @@ public class ARPLaser : MonoBehaviour
         RaycastHit hit;
         if (Physics.Raycast(transform.position, transform.right, out hit))
         {
+            /**
             if (hit.collider.gameObject.tag == "ColliderTest")
             {
                 lr.SetPosition(1, hit.point);
+            }
+
+            */
+
+            switch (hit.collider.gameObject.tag)
+            {
+                case "ColliderTest":
+                    lr.SetPosition(1, hit.point);
+                    break;
+                case "AmpTest":
+                    lr.material = mat2;
+                    break;
             }
         }
         else lr.SetPosition(1, transform.right * 5000);
