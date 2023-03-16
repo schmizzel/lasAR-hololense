ae722ec7ee23e083e8eb4f90061d0b8a6b03f3bb
ChrisOnMac <20260809+schmizzel@users.noreply.github.com>
2023-03-16 12:48:16 +0100

    added new laser script

--- Assets/ARP Scripts/ARPLaserStart.cs
+++ Assets/ARP Scripts/ARPLaserStart.cs
@@ -7,80 +7,23 @@ using UnityEngine;
 public class ARPLaserStart : MonoBehaviour
 {
     public Material mat1, mat2;
-    ARPLaser beam;
     public bool laserActive;
     public Vector3 direction;
     public AudioSource laserSound;
+    LaserNew beam;
 
-    void Start()
-    {
-        UnityEngine.Debug.Log(gameObject.transform.position);
-    }
+    void Start() {}
 
-
-    void Update()
-    {
-        if (laserActive)
-        {
-            Destroy(GameObject.Find("Laser Beam-" + gameObject.name));
-            beam = new ARPLaser(gameObject.transform.position, direction, mat1, mat2, gameObject.name, laserSound); 
-        }
-        else
-        {
-            Destroy(GameObject.Find("Laser Beam-" + gameObject.name));
+    void Update(){
+        if (laserActive) {
+            Destroy(GameObject.Find("Laser Beam"));
+            beam = new LaserNew(gameObject.transform.position, direction, mat1, mat2); 
+        } else {
+            Destroy(GameObject.Find("Laser Beam"));
         }
     }
 
-    public void SetLaserActive(bool input)
-    {
+    public void SetLaserActive(bool input) {
         laserActive = input;
-    }
-
-    public void ActivateLaserStart(string targetobject)
-    {
-        switch (targetobject)
-        {
-            case "portal1":
-                GameObject.Find("Portal1").GetComponent<ARPLaserStart>().SetLaserActive(true);
-                //UnityEngine.Debug.Log("Activated Laser for: " + GameObject.Find("Portal2").name);
-                break;
-            case "portal2":
-                GameObject.Find("Portal2").GetComponent<ARPLaserStart>().SetLaserActive(true);
-                //UnityEngine.Debug.Log("Activated Laser for: " + GameObject.Find("Portal2").name);
-                break;
-            case "amp1":
-                GameObject.Find("Amp1").GetComponent<ARPLaserStart>().SetLaserActive(true);
-                //UnityEngine.Debug.Log("Activated Laser for: " + GameObject.Find("Portal2").name);
-                break;
-            case "amp2":
-                GameObject.Find("Amp2").GetComponent<ARPLaserStart>().SetLaserActive(true);
-                //UnityEngine.Debug.Log("Activated Laser for: " + GameObject.Find("Portal2").name);
-                break;
-        }
-
-
-    }
-
-    public void DeactivateLaserStart(string targetobject)
-    {
-        switch (targetobject)
-        {
-            case "portal1":
-                GameObject.Find("Portal1").GetComponent<ARPLaserStart>().SetLaserActive(false);
-                //UnityEngine.Debug.Log("Activated Laser for: " + GameObject.Find("Portal2").name);
-                break;
-            case "portal2":
-                GameObject.Find("Portal2").GetComponent<ARPLaserStart>().SetLaserActive(false);
-                //UnityEngine.Debug.Log("Activated Laser for: " + GameObject.Find("Portal2").name);
-                break;
-            case "amp1":
-                GameObject.Find("Amp1").GetComponent<ARPLaserStart>().SetLaserActive(false);
-                //UnityEngine.Debug.Log("Activated Laser for: " + GameObject.Find("Portal2").name);
-                break;
-            case "amp2":
-                GameObject.Find("Amp2").GetComponent<ARPLaserStart>().SetLaserActive(false);
-                //UnityEngine.Debug.Log("Activated Laser for: " + GameObject.Find("Portal2").name);
-                break;
-        }
     }
 }
---
+++ Assets/ARP Scripts/LaserNew.cs
@@ -0,0 +1,88 @@
+using UnityEngine;
+
+public class LaserNew {
+    public GameObject laserTarget;
+    Material mat1, mat2;
+    Material currentMaterial;
+    string ignoreTag = "";
+
+    public LaserNew(Vector3 pos, Vector3 dir, Material red, Material green) {
+        this.laserTarget = new GameObject();
+        this.laserTarget.name = "Laser Beam";
+        this.mat1 = red;
+        this.mat2 = green;
+        this.currentMaterial = this.mat1;
+        this.laserTarget.tag = "Laser";
+        addLaserSegment(pos, dir, this.mat1, 0);
+    }
+
+    private void addLaserSegment(Vector3 origin, Vector3 direction, Material mat, int depth) {
+        if (depth > 20) {
+            return;
+        }
+    
+        LineRenderer nextSegment = makeLineRenderer(origin, mat);
+        Ray ray = new Ray(origin, direction);
+        RaycastHit hit; 
+
+        if (closestHit(Physics.RaycastAll(ray), out hit)) {
+            ignoreTag = hit.collider.gameObject.tag;
+            nextSegment.SetPosition(1, hit.point);
+            checkHit(hit, depth + 1);
+        } else {
+            nextSegment.SetPosition(1, direction * 20 + origin);
+        }
+    }
+
+    private void checkHit(RaycastHit hitInfo, int depth) {
+        Vector3 dir = hitInfo.normal * -1;
+        Vector3 origin;
+
+        // TODO: Add a max recursion depth
+        switch (hitInfo.collider.gameObject.tag) {
+            case "AmpTest":
+                this.currentMaterial = mat2;
+                addLaserSegment(hitInfo.point, dir, this.currentMaterial, depth);
+                break;
+            case "Portal1":
+                origin = GameObject.FindWithTag("Portal2").transform.position;
+                addLaserSegment(origin, dir, this.currentMaterial, depth);
+                break;
+            case "Portal2":
+                origin = GameObject.FindWithTag("Portal1").transform.position;
+                addLaserSegment(origin, dir, this.currentMaterial, depth);
+                break;
+        }
+    }
+
+    private LineRenderer makeLineRenderer(Vector3 origin, Material mat) {
+        GameObject child = new GameObject();
+        child.transform.parent = this.laserTarget.transform;
+
+        LineRenderer segment = child.AddComponent(typeof(LineRenderer)) as LineRenderer;    
+        segment.startWidth = 0.02f;
+        segment.endWidth = 0.02f;
+        segment.material = mat;
+        segment.SetPosition(0, origin);
+        return segment;
+    }
+
+    private bool closestHit(RaycastHit[] hits, out RaycastHit closest) {
+        float min = 9999999;
+        bool ok = false;
+        closest = new RaycastHit();
+        foreach (var hit in hits) {
+            if (hit.collider.gameObject.tag == ignoreTag) {
+                continue;
+            }
+
+            if (hit.distance < min) {
+                ok = true;
+                min = hit.distance;
+                closest = hit;
+            }            
+        }
+
+        return ok;
+    }
+}
